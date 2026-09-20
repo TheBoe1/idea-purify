89518a8d6e581fdea2e327faaae674f32b32a47a
Theboe1 <13120895026a@gmail.com>
2026-09-20 08:04:01 +0800

    修改vite命令 启动前端同时拉起node

--- backend/server.ts
+++ backend/server.ts
@@ -1,16 +1,20 @@
 import http from "http";
+import fs from "fs";
 const server = http.createServer((req, res) => {
+    const data = fs.readFileSync("./backend/data/ideas.json", "utf-8");
     if (req.method === "POST" && req.url === "/idea") {
         // Handle POST request to /idea
         let body = '';
-
         req.on('data', chunk => {
             body += chunk.toString();
         });
         req.on('end', () => {
             console.log('Received idea:', body);
             res.writeHead(200, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({ message: "Idea received successfully!" }));
+            res.end(JSON.stringify({ 
+                message: "Idea received successfully!" ,
+                data:body
+            }));
         });
         return;
     }
@@ -25,5 +29,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3000, () => {
-  console.log('Server running on port 3000');
+  console.log('Node Server Successed running ! \nServer running on port 3000');
 });
